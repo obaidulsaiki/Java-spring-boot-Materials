37d179d231e36a2f272dbc1c9277be7477dfc651
obaidulsaiki <22103347@iubat.edu>
2025-04-23 16:38:27 +0600

    Add files via upload

---
+++ Weekand6/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // java collection Framework
+        // Collection - refers to storing the group of objects.
+        // Framework - refers to a ready-made tools that can be used to store the objects
+        // Collection framework means storing a group of datas with a ready-made tools and perform operations.
+        // List (Array list, Hashset. priority queue, ArrayDeque and HashMap)
+        // Set (LinkedList, LinkedHashSet, LinkedList for queue , LinkedList for Deque and LinkedHashMap and Tree map)
+
+        //** LIST ** \\
+        ExmpleOfLst.display();
+    }
+}
