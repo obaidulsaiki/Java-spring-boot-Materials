ce71e8a1b292411710acb0901dfd5a401bbb2ba6
obaidulsaiki <22103347@iubat.edu>
2025-06-18 12:29:58 +0600

    student_management.sql

--- MidTerm Topics/money.sql
+++ MidTerm Topics/money.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 18, 2025 at 08:28 AM
+-- Generation Time: Jun 18, 2025 at 08:29 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -44,6 +44,30 @@ INSERT INTO `money` (`id`, `name`, `money`) VALUES
 (4, 'Diana', 150.00),
 (5, 'Ethan', 12345.67);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `address` varchar(80) NOT NULL,
+  `gmail` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `name`, `address`, `gmail`) VALUES
+(1, 'saiki', 'ssdsdhsudh', 'saki.obidul'),
+(2, 'baiki', 'chorokghata', 'saki.obidul12@gmail.com'),
+(3, 'RifatSalma', 'tangail', 'tipuhossain@gmail.com'),
+(5, 'lsd', 'chandpur', 'lsd@gmail.com'),
+(6, 'Saiket', 'uttara sector 1', 'saikatGain@gmail.com');
+
 --
 -- Indexes for dumped tables
 --
@@ -55,6 +79,12 @@ ALTER TABLE `money`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -63,6 +93,12 @@ ALTER TABLE `money`
 --
 ALTER TABLE `money`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
